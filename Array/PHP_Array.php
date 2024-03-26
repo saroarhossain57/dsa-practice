8cd5d7bb3da1a8d2b811951a003f0f1e8bb5e01c
Saroar Hossain <limonhossain57@gmail.com>
2024-03-26 16:30:55 +0600

    Initial commit

---
+++ Array/PHP_Array.php
@@ -0,0 +1,6 @@
+<?php
+
+$startMemory = memory_get_usage();
+$array = range(1,100000);
+$endMemory = memory_get_usage();
+echo ($endMemory - $startMemory)." bytes";
---
+++ Linked List/Python.py
@@ -0,0 +1,17 @@
+print("Hello Bangla, Welcome to Linked List")
+
+
+LinkedList = [
+    [
+        'data' => {},
+        'next' => {}
+    ]
+]
+
+DoubleLinkedList = [
+    [
+        'data' => {},
+        'next' => {},
+        'prev' => {}
+    ]
+]
