9a22725141054c71247add97cc923a7786b0b656
Saroar Hossain <limonhossain57@gmail.com>
2024-10-26 17:05:17 +0600

    Array Insert

--- tutorialspoint/array/insertion.php
+++ tutorialspoint/array/insertion.php
@@ -2,19 +2,29 @@
 
 $arr = [20, 45, 93, 67, 10, 97, 52, 88, 33, 92];
 
-function insert_item( array &$arr, int | string $position, mixed $item) : array {
-    
+function insert_item(array &$arr, int|string $position, mixed $item): array {
     $count = count($arr);
-
-    if( $position === 'first') {
+    if ($position === 'first') {
         array_unshift($arr, $item);
-    } else if( $position === 'last') {
+    } 
+    elseif ($position === 'last') {
         array_push($arr, $item);
-    } else {
-        for( $i = 0; $i < $count; $i++) {
-            
+    } 
+    elseif (is_int($position) && $position >= 0 && $position <= $count) {
+        $arr[] = null;
+        for ($i = $count; $i > $position; $i--) {
+            $arr[$i] = $arr[$i - 1];
         }
+
+        $arr[$position] = $item;
+    } 
+    else {
+        throw new InvalidArgumentException("Invalid position: $position.");
     }
 
     return $arr;
 }
+
+insert_item($arr, 3, 100);
+
+print_r($arr);
